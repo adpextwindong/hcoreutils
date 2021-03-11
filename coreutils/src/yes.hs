ca850cfb626a45d2f1e67123d25d744960f457fc
Takumi Crary <tcrary@gmail.com>
2021-03-11 22:20:34 +0900

    Switch from strings to text

--- coreutils/src/yes.hs
+++ coreutils/src/yes.hs
@@ -4,26 +4,29 @@ import System.Environment
 import Data.List
 import Data.Either
 import Control.Monad
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
-help = "Usage: yes [STRING]...\n\
-       \  or:  yes OPTION\n\
-       \Repeatedly output a line with all specified STRING(s), or 'y'.\n\
-       \\n\
-       \Haskell coreutils by George Takumi Crary"
 
-version = "yes (Haskellgolf coreutils) 0.0.1\n\
-          \License: BSD-2-Clause\n\
-          \Written by George Takumi Crary"
+help = T.pack "Usage: yes [STRING]...\n\
+              \  or:  yes OPTION\n\
+              \Repeatedly output a line with all specified STRING(s), or 'y'.\n\
+              \\n\
+              \Haskell coreutils by George Takumi Crary"
+
+version = T.pack "yes (Haskellgolf coreutils) 0.0.1\n\
+                 \License: BSD-2-Clause\n\
+                 \Written by George Takumi Crary"
 
 main = sequence_ =<< liftM (loopIfNeeded . buildResp) getArgs
 
-buildResp :: [String] -> Either String String
+buildResp :: [String] -> Either T.Text T.Text
 buildResp ("--help":_) = Right help
 buildResp ("--version":_) = Right version
 
-buildResp [] = Left "y"
-buildResp xs = Left $ intercalate " " xs
+buildResp [] = Left (T.pack "y")
+buildResp xs = Left $ T.pack (intercalate " " xs)
 
-loopIfNeeded :: Either String String -> [IO ()]
-loopIfNeeded (Left resp) = repeat . putStrLn . id $ resp
-loopIfNeeded (Right optionMsg) = [putStr (id optionMsg)]
+loopIfNeeded :: Either T.Text T.Text -> [IO ()]
+loopIfNeeded (Left resp) = repeat . T.putStrLn $ resp
+loopIfNeeded (Right optionMsg) = [T.putStr optionMsg]
