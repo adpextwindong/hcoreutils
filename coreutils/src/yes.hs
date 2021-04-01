e85da5386cfca4d9f5149b32afb07311c5847f36
Takumi Crary <tcrary@gmail.com>
2021-04-01 06:33:14 +0100

    Switch yes to use bufferIO and pack'd Data.Text

--- coreutils/src/yes.hs
+++ coreutils/src/yes.hs
@@ -6,6 +6,7 @@ import Data.Either
 import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import qualified System.IO as SIO
 
 
 help = T.pack "Usage: yes [STRING]...\n\
@@ -18,35 +19,18 @@ version = T.pack "yes (Haskellgolf coreutils) 0.0.1\n\
                  \License: BSD-2-Clause\n\
                  \Written by George Takumi Crary"
 
-main2 = sequence_ =<< liftM (loopIfNeeded . buildResp) getArgs
-
 buildResp :: [String] -> Either T.Text T.Text
-buildResp ("--help":_) = Right help
-buildResp ("--version":_) = Right version
-
-buildResp [] = Left (T.pack "y")
-buildResp xs = Left $ T.pack (intercalate " " xs)
-
-loopIfNeeded :: Either T.Text T.Text -> [IO ()]
-
--- In Data.List repeat is
--- repeat x = xs where xs = x : xs
--- We should see if we're paying the cost for this cons cell and if there a better way
-loopIfNeeded (Left resp) = repeat . T.putStrLn $ resp
-loopIfNeeded (Right optionMsg) = [T.putStr optionMsg]
-
--- Is paying for a `then` operation better than using repeat
-main :: IO ()
-main = join $ liftM (loopDirectIfNeeded . buildResp) getArgs
-
-loopDirectIfNeeded :: Either T.Text T.Text -> IO ()
-loopDirectIfNeeded lr@(Left resp) = T.putStrLn resp >> (loopDirectIfNeeded lr)
-loopDirectIfNeeded (Right optionMsg) = T.putStrLn optionMsg
--- I guess we should buffer IO at this point...
+buildResp ("--help":_)      = Right help
+buildResp ("--version":_)   = Right version
+buildResp []                = Left (T.pack . take bufferSize . cycle $ "y\n")
+buildResp args              = Left $ T.pack (intercalate " " args)
 
 loopForeverLR :: Either T.Text T.Text -> IO ()
-loopForeverLR (Left resp) = forever . T.putStrLn $ resp
+loopForeverLR (Left resp)       = forever . T.putStrLn $ resp
 loopForeverLR (Right optionMsg) = T.putStr optionMsg
 
-main3 : IO ()
-main3 = join $ liftM (loopForeverLR . buildResp) getArgs
+bufferSize = 4096
+
+main = do
+    SIO.hSetBuffering SIO.stdout $ SIO.BlockBuffering (Just bufferSize)
+    join $ liftM (loopForeverLR . buildResp) getArgs
