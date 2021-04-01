d4349919542f0612b658bcf1f35f7c72a70a8bbb
Takumi Crary <tcrary@gmail.com>
2021-04-01 08:14:18 +0100

    Add newtype wrapper around usage messages in Yes

--- coreutils/src/yes.hs
+++ coreutils/src/yes.hs
@@ -7,14 +7,17 @@ import Control.Monad
 import qualified Data.ByteString.Char8 as B
 import qualified System.IO as SIO
 
+newtype UsageMsg = MkUsageMsg B.ByteString
+fromUsageMsg :: UsageMsg -> B.ByteString
+fromUsageMsg (MkUsageMsg bs) = bs
 
-help = B.pack "Usage: yes [STRING]...\n\
+help = MkUsageMsg $ B.pack "Usage: yes [STRING]...\n\
               \  or:  yes OPTION\n\
               \Repeatedly output a line with all specified STRING(s), or 'y'.\n\
               \\n\
               \Haskell coreutils by George Takumi Crary"
 
-version = B.pack "yes (Haskellgolf coreutils) 0.0.1\n\
+version = MkUsageMsg $ B.pack "yes (Haskellgolf coreutils) 0.0.1\n\
                  \License: BSD-2-Clause\n\
                  \Written by George Takumi Crary"
 
@@ -27,15 +30,15 @@ packMsg xs = packedMsg
                     packedMsg = B.pack . take nFit . cycle $ msg
 
 
-buildResp :: [String] -> Either B.ByteString B.ByteString
+buildResp :: [String] -> Either B.ByteString UsageMsg
 buildResp ("--help":_)      = Right help
 buildResp ("--version":_)   = Right version
 buildResp []                = Left (packMsg ["y\n"])
 buildResp args              = Left (packMsg args)
 
-loopForeverLR :: Either B.ByteString B.ByteString -> IO ()
+loopForeverLR :: Either B.ByteString UsageMsg -> IO ()
 loopForeverLR (Left resp)       = forever . B.putStrLn $ resp
-loopForeverLR (Right optionMsg) = B.putStr optionMsg
+loopForeverLR (Right optionMsg) = B.putStr $ fromUsageMsg optionMsg
 
 bufferSize = 4096
 
