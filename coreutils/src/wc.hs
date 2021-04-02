ad2ff52d0f3922b834669d111876109cd5d4af65
Takumi Crary <tcrary@gmail.com>
2021-04-02 06:18:42 +0100

    Fix up the basic main block for simplest using base-4.15 SIO

--- coreutils/src/wc.hs
+++ coreutils/src/wc.hs
@@ -1,20 +1,18 @@
 module Main where
 
 import System.Environment
-import Data.List
-import Control.Applicative
-import Control.Monad
 import qualified Data.ByteString.Char8 as B
 import qualified System.IO as SIO
 
 countLines :: String -> String
-countLines xs = "okay"
+countLines s = s
 
 --TODO check out hFileSize
 linesFile :: FilePath -> IO String
-linesFile fp = SIO.withFile fp SIO.ReadMode ((liftM countLines) . SIO.hGetContents)
+linesFile fp = SIO.withFile fp SIO.ReadMode (fmap countLines . SIO.hGetContents')
 
-main :: IO [IO String]
+main :: IO ()
 main = do
-    files <- liftM (drop 1) getArgs :: IO [FilePath]
-    return $ liftM linesFile files
+    files <- getArgs :: IO [FilePath]
+    acts <- mapM linesFile files
+    mapM_ putStrLn acts
