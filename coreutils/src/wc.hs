c1fb2d2bd29e0730b6b3fa43a083a7bca19ca0bd
Takumi Crary <tcrary@gmail.com>
2021-04-02 07:36:16 +0100

    Add basic wc, still has linebreak types issues

--- coreutils/src/wc.hs
+++ coreutils/src/wc.hs
@@ -1,18 +1,29 @@
 module Main where
 
 import System.Environment
+import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as B
 import qualified System.IO as SIO
 
-countLines :: String -> String
-countLines s = s
+countLines = (-1 +) . length . B.lines
+countWords = length . B.words
+countBytes = B.length
 
---TODO check out hFileSize
-linesFile :: FilePath -> IO String
-linesFile fp = SIO.withFile fp SIO.ReadMode (fmap countLines . SIO.hGetContents')
+wcBS :: ByteString -> (Int, Int, Int)
+wcBS s = (cLines, cWords, cBytes)
+    where 
+        cLines = countLines s
+        cWords = countWords s
+        cBytes = countBytes s
+
+totalLCB :: [(Int,Int,Int)] -> (Int,Int,Int)
+totalLCB = foldl (\(a,b,c) (x,y,z) -> (a+x, b+y, c+z)) (0,0,0)
 
 main :: IO ()
 main = do
-    files <- getArgs :: IO [FilePath]
-    acts <- mapM linesFile files
-    mapM_ putStrLn acts
+    fps <- getArgs
+    files <- mapM B.readFile fps :: IO [ByteString]
+    let results = fmap wcBS files :: [(Int, Int, Int)]
+    let total = totalLCB results
+    sequence_ $ fmap putStrLn $ fmap show $ zip results fps
+    putStrLn $ show (total, "total")
