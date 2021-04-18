583e5822cd03ce59d93cb9f03f053221495ffcb9
Takumi Crary <tcrary@gmail.com>
2021-04-19 02:12:11 +0900

    Add WIP work on interp in echo

--- src/echo.hs
+++ src/echo.hs
@@ -5,6 +5,7 @@ import Data.Monoid
 import Options.Applicative
 import Data.Bool
 import Data.List
+import qualified Data.Set as Set
 
 data EchoOpts = EchoOpts {
                     noTrailingNewline :: Bool,
@@ -25,6 +26,7 @@ argpNoTrailingNewline :: Parser Bool
 argpNoTrailingNewline = switch ( short 'n' <>
            help "Do not output the trailing newline")
 
+argpInterpret :: Parser Bool
 argpInterpret = flag False True ( short 'e' <>
            help "enable interpretation of backslash escapes")
 
@@ -34,6 +36,7 @@ argpDisableBackslashInterpret = flag' True ( short 'E' <>
 
 argpEchos :: Parser [String]
 argpEchos = many ( argument str (metavar "STRINGS..." ))
+
 optsParse :: ParserInfo EchoOpts
 optsParse =
     info (helper <*> versionOption <*> echoOptParse)
@@ -55,8 +58,47 @@ main = do
 
 main' :: EchoOpts -> IO ()
 main' (EchoOpts _ _ []) = return ()
-main' (EchoOpts pTrail False xs) = do
-    mapM_ putStr $ intersperse " " xs
+
+main' (EchoOpts pTrail pInterpretBackslashes xs) = do
+    let results = if pInterpretBackslashes
+                  then handleBackslashes <$> xs
+                  else putStr <$> xs
+    sequence_ $ intersperse (putStr " ") results
     putStr $ bool "\n" "" pTrail
 
-main' (EchoOpts pTrail pInterpretBackslashes xs) = undefined --TODO backslash handling
+
+handleBackslashes :: String -> IO ()
+handleBackslashes [] = return ()
+handleBackslashes (x:[]) = putStr [x]
+handleBackslashes s@(x:xs) = if pStringLiteral s
+                             then interpretAsEscaped s
+                             else putStr s
+    where pStringLiteral ts = head ts == '"' && last ts == '"' ||
+                             head ts == '\'' && last ts == '\''
+
+interpretAsEscaped :: String -> IO () -- TODO perform the print here correctly
+interpretAsEscaped [] = return ()
+interpretAsEscaped ('\\':'\\':xs) = putChar '\\'
+{-
+interpretAsEscaped ('\\':xs) = mapping (head xs) >> interpretAsEscaped (tail xs) -- skip the next character
+    where
+        mapping x = if Set.member x interpSet
+                  then putChar $ escMap x
+                  else putChar x
+-}
+interpretAsEscaped s = putStr regular >> interpretAsEscaped startsWithEscaped
+    where (regular,startsWithEscaped) = break (== '\\') s
+
+interpSet = Set.fromList ['\\','a','b','f','n','r','t','v']
+
+escMap 'a' = '\a'
+escMap 'b' = '\b'
+escMap 'f' = '\f'
+escMap 'n' = '\n'
+escMap 'r' = '\r'
+escMap 't' = '\t'
+escMap 'v' = '\v'
+escMap c = c
+
+--TODO handle octal/hexadecimal
+-- Seems like \c in any interpretted string kills all further output which means we need to recurse on the args instead of mapM zzz
