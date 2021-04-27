c8592ca1562908b43f019547a3c19ae33d073924
Takumi Crary <tcrary@gmail.com>
2021-04-27 23:20:40 +0900

    Refactor uniq option parsing mutually exclusive flags

--- src/uniq.hs
+++ src/uniq.hs
@@ -6,7 +6,7 @@ import Options.Applicative
 import Control.Monad
 -- import Data.Map
                                                 --NoSeperation for allDups
-data DuplicateHandling = NoDups | OneEachDups | AllDups | AllRepeated SeperationHandling
+data DuplicateHandling = NoDups | OneEachDups | AllDups | AllRepeated SeperationHandling | Grouped GroupHandling | Unique
     deriving Show
 
 --Maybe these clash
@@ -19,26 +19,22 @@ data UniqOpts = UniqOpts {                                  -- Corresponding GNU
                     outputCount      :: Bool,               -- -c --count
                     outputDuplicates :: DuplicateHandling,  -- -d --all-repeated
                     skipFields       :: Int,                -- -f --skip-fields=N
-                    groupMethod      :: GroupHandling,      -- --group[=METHOD]
                     ignoreCase       :: Bool,               -- -i --ignore-case
                     skipChars        :: Int,                -- -s --skip-chars=N
-                    uniqueOnly       :: Bool,               -- -u --unique
                     nullTerminated   :: Bool,               -- -z --zero-terminated
                     checkChars       :: Maybe Int           -- -w --check-chars=N
                 }deriving Show
 
 defaultOpts :: UniqOpts
-defaultOpts = UniqOpts False NoDups 0 SeparateGroups False 0 False False Nothing
+defaultOpts = UniqOpts False NoDups 0 False 0 False Nothing
 
 optsParser :: Parser UniqOpts
 optsParser = UniqOpts
             <$> outputCountParser
             <*> outputDuplicatesParser
             <*> skipFieldsParser
-            <*> groupHandlingParser
             <*> ignoreCaseParser
             <*> skipCharsParser
-            <*> uniqueOnlyParser
             <*> nullTerminatedParser
             <*> checkCharsParser
 
@@ -47,10 +43,11 @@ outputCountParser = switch ( short 'c' <> long "count" <> help "TODO prefix line
 
 -- -d -D --all-Repeated[GROUPS]
 outputDuplicatesParser :: Parser DuplicateHandling
-outputDuplicatesParser = allDupsParser <|> oneEachDupsParser <|> allRepeatedParser
+outputDuplicatesParser = allDupsParser <|> oneEachDupsParser <|> allRepeatedParser <|> groupHandlingParser <|> uniqueOnlyParser
     where
         allDupsParser = flag NoDups AllDups ( short 'D' )
         oneEachDupsParser = flag' OneEachDups ( short 'd' )
+        uniqueOnlyParser = flag' Unique ( short 'u' <> long "unique" <> help "TODO Only print unique lines" )
 
 
 allRepeatedParser :: Parser DuplicateHandling
@@ -61,18 +58,18 @@ allRepeatedParser = defaultRepeatedParser <|> noneRepeatedParser <|> prependPars
         prependParser = flag' (AllRepeated Prepend) ( long "long-repeated=prepend" )
         separateParser = flag' (AllRepeated Separate) (long "long-repeated=separate" )
 
+
+groupHandlingParser :: Parser DuplicateHandling
+groupHandlingParser = separateGroupsParser <|> prependGroupsParser <|> appendGroupsParser <|> bothGroupsParser
+    where
+        separateGroupsParser = flag' (Grouped SeparateGroups) ( long "group=separate" )
+        prependGroupsParser  = flag' (Grouped PrependGroups) ( long "group=prepend" )
+        appendGroupsParser   = flag' (Grouped AppendGroups) ( long "group=append" )
+        bothGroupsParser     = flag' (Grouped BothGroups) ( long "group=both" )
+
+
 skipFieldsParser :: Parser Int
 skipFieldsParser = option auto ( short 'f' <> long "skip-fields" <> value 0 <> help "TODO avoid compariong the first N fields" )
-
-groupHandlingParser :: Parser GroupHandling
-groupHandlingParser = defaultGroupHandlingParser <|> separateGroupsParser <|> prependGroupsParser <|> appendGroupsParser <|> bothGroupsParser
-    where
-        defaultGroupHandlingParser = flag NoGroups SeparateGroups ( long "group" )
-        separateGroupsParser = flag' SeparateGroups ( long "group=separate" )
-        prependGroupsParser = flag' PrependGroups ( long "group=prepend" )
-        appendGroupsParser = flag' AppendGroups ( long "group=append" )
-        bothGroupsParser = flag' BothGroups ( long "group=both" )
-
 
 ignoreCaseParser :: Parser Bool
 ignoreCaseParser = switch ( short 'i' <> long "ignore-case" <> help "TODO ignore differences in case when comparing" )
@@ -80,8 +77,6 @@ ignoreCaseParser = switch ( short 'i' <> long "ignore-case" <> help "TODO ignore
 skipCharsParser :: Parser Int
 skipCharsParser = option auto ( value 0 <> short 's' <> long "skip-chars" <> help "TODO avoid comparing the first N characters" )
 
-uniqueOnlyParser :: Parser Bool
-uniqueOnlyParser = switch ( short 'u' <> long "unique" <> help "TODO Only print unique lines" )
 
 nullTerminatedParser :: Parser Bool
 nullTerminatedParser = switch ( short 'z' <> long "zero-temrinated" <> help "TODO line delimiter is NUL, not lewline")
