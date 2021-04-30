96c94f9b170619b6bdfd8733bdc1bae7eb31a0f5
Takumi Crary <tcrary@gmail.com>
2021-04-30 22:29:26 +0900

    Hlint and switch to freebsd style options for uniq

--- src/uniq.hs
+++ src/uniq.hs
@@ -9,23 +9,20 @@ import Data.List
 import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
 
-data DuplicateHandling = NoDups | -- default
+data DuplicateHandling = NoDups | -- default -u
                          OneEachDups | -- d
                          AllDups | -- D
-                         AllRepeated SeperationHandling |
-                         Grouped GroupHandling
+                         AllRepeated SeperationHandling --all-repeated
+
     deriving Show
 
 --Maybe these clash
 data SeperationHandling = Prepend | Separate
     deriving Show
-data GroupHandling = NoGroups | SeparateGroups | PrependGroups | AppendGroups | BothGroups
-    deriving Show
 
 data UniqOpts = UniqOpts {                                  -- Corresponding GNU Uniq flags
                     outputCount      :: Bool,               -- -c --count
                     outputDuplicates :: DuplicateHandling,  -- -d --all-repeated
-                    uniqueHandling   :: Bool,
                     skipFields       :: Int,                -- -f --skip-fields=N
                     ignoreCase       :: Bool,               -- -i --ignore-case
                     skipChars        :: Int,                -- -s --skip-chars=N
@@ -34,13 +31,12 @@ data UniqOpts = UniqOpts {                                  -- Corresponding GNU
                 }deriving Show
 
 defaultOpts :: UniqOpts
-defaultOpts = UniqOpts False NoDups False 0 False 0 False Nothing
+defaultOpts = UniqOpts False NoDups 0 False 0 False Nothing
 
 optsParser :: Parser UniqOpts
 optsParser = UniqOpts
             <$> outputCountParser
             <*> outputDuplicatesParser
-            <*> uniqueHandlingParser
             <*> skipFieldsParser
             <*> ignoreCaseParser
             <*> skipCharsParser
@@ -52,10 +48,11 @@ outputCountParser = switch ( short 'c' <> long "count" <> help "TODO prefix line
 
 -- -d -D --all-Repeated[GROUPS]
 outputDuplicatesParser :: Parser DuplicateHandling
-outputDuplicatesParser = allDupsParser <|> oneEachDupsParser <|> allRepeatedParser <|> groupHandlingParser
+outputDuplicatesParser = allDupsParser <|> oneEachDupsParser <|> allRepeatedParser <|> noDupesParser
     where
         allDupsParser = flag NoDups AllDups ( short 'D' )
         oneEachDupsParser = flag' OneEachDups ( short 'd' )
+        noDupesParser = flag' NoDups ( short 'u' <> long "unique" <> help "TODO Only print unique lines if no duplicate handling is involved" )
 
 
 allRepeatedParser :: Parser DuplicateHandling
@@ -66,17 +63,6 @@ allRepeatedParser = defaultRepeatedParser <|> noneRepeatedParser <|> prependPars
         prependParser = flag' (AllRepeated Prepend) ( long "long-repeated=prepend" )
         separateParser = flag' (AllRepeated Separate) (long "long-repeated=separate" )
 
-
-groupHandlingParser :: Parser DuplicateHandling
-groupHandlingParser = separateGroupsParser <|> prependGroupsParser <|> appendGroupsParser <|> bothGroupsParser
-    where
-        separateGroupsParser = flag' (Grouped SeparateGroups) ( long "group=separate" )
-        prependGroupsParser  = flag' (Grouped PrependGroups) ( long "group=prepend" )
-        appendGroupsParser   = flag' (Grouped AppendGroups) ( long "group=append" )
-        bothGroupsParser     = flag' (Grouped BothGroups) ( long "group=both" )
-
-uniqueHandlingParser :: Parser Bool
-uniqueHandlingParser = switch ( short 'u' <> long "unique" <> help "TODO Only print unique lines if no duplicate handling is involved" )
 
 skipFieldsParser :: Parser Int
 skipFieldsParser = option auto ( short 'f' <> long "skip-fields" <> value 0 <> help "TODO avoid compariong the first N fields" )
@@ -126,7 +112,8 @@ main = do
                         Just fp -> openFile fp WriteMode
                         Nothing -> return stdout
 
-    main' (appOpts args) inHandle outHandle
+    content <- hGetContents' inHandle
+    main' (appOpts args) content outHandle
     exitSuccess
 
 
@@ -138,7 +125,7 @@ tossSeqRepeat (x:y:xs) = if x == y
 tossSeqRepeat x = x
 
 skipNfields :: Int -> String -> String
-skipNfields n s = (iterate dropField s) !! n
+skipNfields n s = iterate dropField s !! n
 --TODO see if gnu uniq drops leading white space after dropping the fields
 
 dropField :: String -> String
@@ -185,24 +172,23 @@ instance Semigroup Occurance where
 lineOccurances :: UniqOpts -> [String] -> Map.Map String Occurance
 lineOccurances opts xs = Map.fromListWith (<>) $ toUniqueSingle <$> zip xs [1..]
     where
-        toUniqueSingle = (\(s,ln) -> ((limitedString opts) s, Unique ln))
+        toUniqueSingle = \(s,ln) -> (limitedString opts s, Unique ln)
 
 filterUniqueOccs :: [Occurance] -> [Occurance]
-filterUniqueOccs xs = [x | x@(Unique {}) <- xs]
+filterUniqueOccs xs = [x | x@Unique {} <- xs]
 
 filterDuplicatedOccs :: [Occurance] -> [Occurance]
-filterDuplicatedOccs xs = [x | x@(Duplicated {}) <- xs]
+filterDuplicatedOccs xs = [x | x@Duplicated {} <- xs]
 
 addOccurance :: Occurance -> LineNumber -> Occurance
 addOccurance (Unique fstln) newln = Duplicated [fstln,newln]
 addOccurance (Duplicated xs) newln = Duplicated $ newln:xs
 
-main' :: UniqOpts -> Handle -> Handle -> IO ()
-main' defaultOpts inHandle outHandle = do
-    mergedLines <- tossSeqRepeat . lines <$> hGetContents' inHandle
-    forM_ mergedLines $ \line -> do
-        hPutStrLn outHandle line
+main' :: UniqOpts -> String -> Handle -> IO ()
+main' defaultOpts content outHandle = do
+    let mergedLines = tossSeqRepeat . lines $ content
+    mapM_ (hPutStrLn outHandle) mergedLines
 
-main' opts inHandle outHandle = do
-    let uniquenessMap = lineOccurances opts . lines <$>  hGetContents' inHandle
+main' opts content outHandle = do
+    let uniquenessMap = lineOccurances opts . lines $ content -- keyed by the limitedView of the string
     return ()
